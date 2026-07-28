35b713296cebe03a93d4495135b8563c63d1deb5
andmailg <55160785+andmailg@users.noreply.github.com>
2026-07-28 09:54:29 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -482,10 +482,10 @@ def main():
   }
     }
 
-    with open("config.json", "w", encoding="utf-8") as f:
+    with open("BLACK_SS+All_RUS.json", "w", encoding="utf-8") as f:
         json.dump(singbox_config, f, ensure_ascii=False, indent=2)
 
-    print(f"Successfully generated config.json with {len(outbounds)} nodes.")
+    print(f"Successfully generated BLACK_SS+All_RUS.json with {len(outbounds)} nodes.")
 
 
 if __name__ == "__main__":
