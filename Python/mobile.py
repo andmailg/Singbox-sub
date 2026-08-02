f79ce5c83db9c1829029b584a9821ccd5c8eff14
andmailg <55160785+andmailg@users.noreply.github.com>
2026-08-02 19:27:57 +0300

    Update mobile.py

--- Python/mobile.py
+++ Python/mobile.py
@@ -415,7 +415,6 @@ def main():
             "action": "hijack-dns"
         },
         {
-            // 1. Сначала точечно перехватываем списки (домены и IP)
             "rule_set": [
                 "db-antizapret",
                 "db-category-ai-chat"
@@ -423,7 +422,6 @@ def main():
             "outbound": "proxy-out"
         },
         {
-            // 2. Затем пускаем ВСЕ российские сайты НАПРЯМУЮ
             "rule_set": [
                 "geosite-category-ru",
                 "geoip-ru"
@@ -431,7 +429,6 @@ def main():
             "outbound": "direct-out"
         },
         {
-            // 3. СРАЗУ ПОСЛЕ ЭТОГО перехватываем оставшийся FakeIP
             "ip_cidr": [
                 "198.18.0.0/15",
                 "fc00::/18"
@@ -439,7 +436,6 @@ def main():
             "outbound": "proxy-out"
         },
         {
-            // 4. И только в самом конце обрабатываем протоколы
             "protocol": "quic",
             "outbound": "proxy-out"
         }
