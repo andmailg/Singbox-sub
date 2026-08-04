003f8db0f8d9d2ab0b676d3fbc60d0c20ae196a0
andmailg <55160785+andmailg@users.noreply.github.com>
2026-08-04 18:44:28 +0300

    Update mobile.py

--- Python/mobile.py
+++ Python/mobile.py
@@ -482,7 +482,7 @@ def main():
         "download_detour": "direct-out"
       }
     ],
-      "final": "direct-out",
+      "final": "proxy-out",
       "auto_detect_interface": True,
       "override_android_vpn": True,
       "default_domain_resolver": "dns-local"
