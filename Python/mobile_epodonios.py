b6f2c2744f30eed37022ddf7c77a30ffea4e8dce
andmailg <55160785+andmailg@users.noreply.github.com>
2026-08-06 11:04:58 +0300

    Update mobile_epodonios.py

--- Python/mobile_epodonios.py
+++ Python/mobile_epodonios.py
@@ -51,7 +51,7 @@ def parse_proxy_link(link: str) -> dict | None:
     if scheme == "vless":
         # Фильтрация по портам (Строго 443 или 8443)
         port = parsed.port
-        if port not in:
+        if port not in [443, 8443]:
             print(f"Skipping VLESS node: invalid port {port} (Only 443/8443 allowed) for tag '{tag}'")
             return None
 
