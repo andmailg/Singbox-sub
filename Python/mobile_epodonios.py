d8c72bb6cd53d514bb88d680da494242f3a36946
andmailg <55160785+andmailg@users.noreply.github.com>
2026-08-06 06:49:44 +0300

    Update mobile_epodonios.py

--- Python/mobile_epodonios.py
+++ Python/mobile_epodonios.py
@@ -398,7 +398,7 @@ def main():
             outbounds.append(outbound)
 
     # --- 3. УМНЫЙ РАЗБРОС (ВЫБОРКА ИЗ НАЧАЛА, СЕРЕДИНЫ И КОНЦА) ---
-    MAX_NODES_LIMIT = 5000
+    MAX_NODES_LIMIT = 150
     total_found = len(outbounds)
 
     if total_found > MAX_NODES_LIMIT:
