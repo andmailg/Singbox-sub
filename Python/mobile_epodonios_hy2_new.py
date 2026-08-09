081f4af26dca583f4b0ec93cd03472f3868bb979
andmailg <55160785+andmailg@users.noreply.github.com>
2026-08-09 18:42:54 +0300

    Update mobile_epodonios_hy2_new.py

--- Python/mobile_epodonios_hy2_new.py
+++ Python/mobile_epodonios_hy2_new.py
@@ -619,7 +619,7 @@ def main():
     with open("sing-box-epodonios-hy2-new.json", "w", encoding="utf-8") as f:
         json.dump(singbox_config, f, ensure_ascii=False, indent=2)
 
-    print(f"Successfully generated sing-box-epodonios.json with {len(outbounds)} nodes.")
+    print(f"Successfully generated sing-box-epodonios-hy2-new.json with {len(outbounds)} nodes.")
 
 
 if __name__ == "__main__":
