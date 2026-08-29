9d4c4b0ded7e5724b3129c189cfa8e1b17d66f64
andmailg <55160785+andmailg@users.noreply.github.com>
2026-08-29 22:09:52 +0300

    Update mobile-hy2_gigachat.py

--- Python/mobile-hy2_gigachat.py
+++ Python/mobile-hy2_gigachat.py
@@ -371,8 +371,8 @@ def main():
         ],
         "http_clients": [
             {
-                "tag": "rules-downloader",
-                "detour": "direct-out"
+                "tag": "rules-downloader"
+                //"detour": "direct-out"
             }
         ],
         "route": {
