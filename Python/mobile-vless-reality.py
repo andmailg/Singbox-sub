d1b7b17f7cf1fa122cc36bd30d3a97f211d8206d
andmailg <55160785+andmailg@users.noreply.github.com>
2026-08-31 09:42:05 +0300

    Update mobile-vless-reality.py

--- Python/mobile-vless-reality.py
+++ Python/mobile-vless-reality.py
@@ -445,7 +445,7 @@ def main():
     with open("sing-box-vless.json", "w", encoding="utf-8") as f:
         json.dump(singbox_config, f, ensure_ascii=False, indent=2)
 
-    print(f"Successfully generated sing-box-vless.json with {len(outbounds)} nodes.")
+    print(f"Successfully generated sing-box-vless-reality.json with {len(outbounds)} nodes.")
 
 
 if __name__ == "__main__":
