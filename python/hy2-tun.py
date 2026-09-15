099787da95060b2ad846e3cd608ef152b5873f08
andmailg <andmailg@gmail.com>
2026-09-15 14:22:43 +0300

    fix

--- python/hy2-tun.py
+++ python/hy2-tun.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     run_pipeline(
         parser_module="src.parsers.hy2_parser",
         exporter="tun",
-        output_file="hy2.json",
+        output_file="hy2-tun.json",
     )
