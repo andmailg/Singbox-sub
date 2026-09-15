1e963ae2588a4d355023a9c047683fce000605d8
andmailg <andmailg@gmail.com>
2026-09-15 14:06:34 +0300

    fix

--- python/router-hy2-sb.py
+++ python/router-hy2-sb.py
@@ -6,5 +6,6 @@ from src.orchestrator import run_pipeline
 if __name__ == "__main__":
     run_pipeline(
         parser_module="src.parsers.hy2_parser",
+        exporter="router",
         output_file="config.json",
     )
